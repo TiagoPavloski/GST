34bf4f804cb0d3570e116c9b92cfb9a4a1855fd0
patriciasdm <patricia_dellamatta@hotmail.com>
2017-10-20 22:23:20 -0200

    Corrigido Ninject de Usuario e mensagem de erro no login

--- Projeto/GST/src/BI.GST.Application/AppService/AutoMapper/ViewModelToDomainMappingProfile.cs
+++ Projeto/GST/src/BI.GST.Application/AppService/AutoMapper/ViewModelToDomainMappingProfile.cs
@@ -63,6 +63,7 @@ namespace BI.GST.Application.AutoMapper
             Mapper.CreateMap<CBOViewModel, CBO>();
             Mapper.CreateMap<FuncionarioEmpresaViewModel, FuncionarioEmpresa>();
             Mapper.CreateMap<InstituicaoCursoViewModel, InstituicaoCurso>();
-        }
+			Mapper.CreateMap<UsuarioViewModel, Usuario>();
+		}
     }
 }
